f60e2038c44ad944b04636c50cf2c8ee2039faab
Denis Semyannikov <Denny32191@gmail.com>
2023-12-18 02:56:56 +0200

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/VueTestingREgister>/' // Замените <имя-репозитория> на имя вашего репозитория GitHub
+    : '/'
+}
